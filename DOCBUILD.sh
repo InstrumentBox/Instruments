fba5a38d69681cc0cf6f5d09c37c2c9a178a31c6
Aleksei Zaikin <aleksi.zaikin@gmail.com>
2024-04-07 01:34:07 +0300

    Update Swift version to 5.10

--- DOCBUILD.sh
+++ DOCBUILD.sh
@@ -33,7 +33,7 @@ PACKAGE_NAME="Instruments"
 xcodebuild docbuild \
 -scheme $PACKAGE_NAME \
 -derivedDataPath .docbuild \
--destination 'platform=iOS Simulator,name=iPhone 14 Pro'
+-destination 'platform=iOS Simulator,name=iPhone 15 Pro'
 
 DOC_PATH=$(find .docbuild -type d -name "$PACKAGE_NAME.doccarchive")
 
